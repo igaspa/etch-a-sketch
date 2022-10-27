4f35004f0bb9b2864e838ae2ae4f1e98984f571f
igaspa <gasparovivana@outlook.com>
2022-10-27 12:03:08 +0200

    Set Mode and change Colors

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,13 @@
+let CONST_COLOR = 'black';
+let mode = 'black'
+
 const deleteBtn = document.querySelector('#deleteButton')
 const colorBtn = document.querySelector('#colorButton')
 const blackBtn = document.querySelector('#blackButton')
 const tenBtn = document.querySelector('#tenPix')
 const twentyBtn = document.querySelector('#twentyPix')
 const thirtyBtn = document.querySelector('#thirtyPix')
+const resetBtn = document.querySelector('#resetButton')
 
 let gridContainer = document.querySelector('#mainField');
 
@@ -11,12 +15,18 @@ tenBtn.addEventListener('click', pixelSize)
 twentyBtn.addEventListener('click', pixelSize)
 thirtyBtn.addEventListener('click', pixelSize)
 
+deleteBtn.addEventListener('click', setMode);
+blackBtn.addEventListener('click', setMode);
+colorBtn.addEventListener('click', setMode);
+resetBtn.addEventListener('click', reset);
+
 function makeRows(rows, cols) {
-    //add property value for rows and cols
     gridContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
     gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
     for (let i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        cell.addEventListener('mouseover', changeColor);
         gridContainer.appendChild(cell);
     };
 };
@@ -31,11 +41,32 @@ function pixelSize(event) {
         makeRows(30, 30)
 }
 
-const square = document.querySelector('#mainField');
-square.addEventListener('mouseover', function (event) {
-    event.target.style.backgroundColor = 'black';
-});
+function setMode(event) {
+    mode = event.target.value;
+    if (mode === 'erase')
+        CONST_COLOR = 'white'
+    else if (mode === 'rainbow') {
+        let randomColor = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+        CONST_COLOR = `#${randomColor}`
+    }
+    else if (mode === 'black')
+        CONST_COLOR = 'black'
+}
 
+function changeColor(event) {
+    if (mode === 'rainbow') {
+        let randomColor = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+        CONST_COLOR = `#${randomColor}`
+        event.target.style.backgroundColor = CONST_COLOR
+    } else {
+        event.target.style.backgroundColor = CONST_COLOR
+    }
+}
 
-
-
+function reset() {
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(box => {
+        box.style.backgroundColor = 'white';
+    });
+    // }
+}
